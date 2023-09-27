0fde250646322da1e375fceff515ed99fe266a2c
audricdet <audric.det@hotmail.com>
2023-09-27 15:37:18 +0200

    add clerkAuth

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -11,6 +11,6 @@ export const exampleRouter = createTRPCRouter({
       };
     }),
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.user.findMany();
+    return ctx.db.example.findMany();
   }),
 });
